8278270afbfcbe464d732cfa6774acaeb116aa02
oasemota99 <45344570+oasemota99@users.noreply.github.com>
2020-07-07 11:08:30 -0500

    Update Event.java

--- capstone/src/main/java/com/step902020/capstone/Event.java
+++ capstone/src/main/java/com/step902020/capstone/Event.java
@@ -2,8 +2,8 @@ package com.step902020.capstone;
 
 import org.springframework.cloud.gcp.data.datastore.core.mapping.Entity;
 import org.springframework.cloud.gcp.data.datastore.core.mapping.Field;
+import org.springframework.data.annotation.Reference;
 import org.springframework.data.annotation.Id;
-import java.time.LocalDateTime;
 import java.util.*;
 
 @Entity(name = "event")
@@ -12,7 +12,7 @@ public class Event{
   @Id
   Long datastoreID;
 
-  @Field(name="organizationName")
+  @Reference
   String organizationName;
 
   @Field(name="eventTitle")
@@ -24,43 +24,20 @@ public class Event{
   @Field(name="eventDescription")
   String eventDescription;
 
-  // @Field(name="university")
-  // String university;
-
   @Field(name="eventLatitude")
   double eventLatitude;
 
   @Field(name="eventLongitude")
   double eventLongitude;
 
-  // @Field(name="eventFilters")
-  // List<String> eventFilters;
-  
-  // public Event(Long datastoreID, Long organizationID, String organizationName, String eventTitle, LocalDateTime eventDateTime, String university, String eventDescription, Long eventLatitude, Long eventLongitude, List<String> eventFilters) {
-    
-  //   this.datastoreID = datastoreID;
-  //   this.organizationID = organizationID;
-  //   this.organizationName = organizationName;
-  //   this.eventTitle = eventTitle;
-  //   this.eventDateTime = eventDateTime;
-  //   this.eventDescription = eventDescription;
-  //   this.university = university;
-  //   this.eventLatitude = eventLatitude;
-  //   this.eventLongitude = eventLongitude;
-  //   this.eventFilters = eventFilters;
-  // }
-
     public Event(String organizationName, String eventTitle,String eventDateTime, String eventDescription, double eventLatitude, double eventLongitude) {
-    
-    this.organizationName = organizationName;
+ 
+    this.organizationName = organizationName;  
     this.eventTitle = eventTitle;
     this.eventDateTime = eventDateTime;
-    //this.university = university;
     this.eventDescription = eventDescription;
     this.eventLatitude = eventLatitude;
     this.eventLongitude = eventLongitude;
-    //this.eventFilters = eventFilters;
-
   }
 
   public Long getDatastoreID() {
@@ -73,23 +50,17 @@ public class Event{
   public String getOrganizationName() {
     return organizationName;
   }
-  public String geteventDateTime() {
+  public String getEventDateTime() {
     return eventDateTime;
   }
   public String getDescription() {
     return eventDescription;
   }
-  // public String getuniversity() {
-  //   return university;
-  // }
   public double getEventLatitude() {
     return eventLatitude;
   }
-  public double geteventLongitude() {
+  public double getEventLongitude() {
     return eventLongitude;
   }
-  // public List<String> getEventFilters() {
-  //   return eventFilters;
-  // }
 
 }
