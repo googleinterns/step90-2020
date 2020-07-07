cd862d140363f02e82f6e4133c09c876117d891e
oasemota99 <45344570+oasemota99@users.noreply.github.com>
2020-07-07 11:31:40 -0500

    Update event.js

--- capstone/src/main/resources/static/event.js
+++ capstone/src/main/resources/static/event.js
@@ -10,39 +10,20 @@ function createEvent(event) {
   const listEventElement = document.createElement('li');
   listEventElement.className = 'list-events';
 
-  // const organizationID = document.createElement('p');
-  // organizationID.innerText = event.organizationID;
-  // listEventElement.appendChild(organizationID);
-
-  const organizationName = document.createElement('p');
-  organizationName.innerText = event.organizationName;
-  listEventElement.appendChild(organizationName);
-
-  const eventTitle = document.createElement('p');
-  eventTitle.innerText = event.eventTitle;
-  listEventElement.appendChild(eventTitle);
-
-  const eventDateTime = document.createElement('p');
-  eventDateTime.innerText = event.eventDateTime;
-  listEventElement.appendChild(eventDateTime);
-
-  const eventDescription = document.createElement('p');
-  eventDescription.innerText = event.eventDescription;
-  listEventElement.appendChild(eventDescription);
-
-  // const university = document.createElement('p');
-  // university.innerText = event.university;
-  // listEventElement.appendChild(university);
-
-  const eventLatitude = document.createElement('p');
-  eventLatitude.innerText = event.eventLatitude;
-  listEventElement.appendChild(eventLatitude);
-
-  const eventLongitude = document.createElement('p');
-  eventLongitude.innerText = event.eventLongitude;
-  listEventElement.appendChild(eventLongitude);
+  createEventAttribute(organizationName);
+  createEventAttribute(eventTitle);
+  createEventAttribute(eventDateTime);
+  createEventAttribute(eventDescription);
+  createEventAttribute(eventLatitude);
+  createEventAttribute(eventLongitude);
   
   return listEventElement;
+}
+
+function createEventAttribute(attributeName) {
+  attributeName = document.createElement('p');
+  atttributeName.innerText = event.attributeName;
+  listEventElement.appendChild(attributeName);
 }
 
 async function newEvent() {
