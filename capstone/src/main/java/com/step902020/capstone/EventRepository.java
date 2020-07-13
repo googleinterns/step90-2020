bd686a3fa19a3a705043eefe24514ace4e9d2992
oasemota99 <45344570+oasemota99@users.noreply.github.com>
2020-07-13 08:37:41 -0500

    Update EventRepository.java

--- capstone/src/main/java/com/step902020/capstone/EventRepository.java
+++ capstone/src/main/java/com/step902020/capstone/EventRepository.java
@@ -1,7 +1,6 @@
 package com.step902020.capstone;
 
 import org.springframework.cloud.gcp.data.datastore.repository.DatastoreRepository;
-import org.springframework.core.convert.converter.Converter;
 /**
  * Spring Data Repository for Event Entities
  */
