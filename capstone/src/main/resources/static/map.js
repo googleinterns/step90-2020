c8b19f3645aeea9ae141598d77a615e239b4d5f0
oasemota99 <oasemota@google.com>
2020-07-21 11:46:27 -0500

    add marker click placement

--- capstone/src/main/resources/static/map.js
+++ capstone/src/main/resources/static/map.js
@@ -4,40 +4,25 @@ async function createMap() {
   const campusMap = new google.maps.Map(
     document.getElementById('map'),
     {center: princetonLatLng, zoom: 16});
-    infoWindow = new google.maps.InfoWindow;
+
+   google.maps.event.addListener(campusMap, 'click', function(event) {
+    placeMarkerAndPan(event.latLng, campusMap);
+   });
 
    const response = await fetch('get-all-events');
    const jsonEvents = await response.json();
    jsonEvents.forEach(event => createMarker(event, campusMap));
 
-/* Geolocation API */
-
-//    if (navigator.geolocation) {
-//      navigator.geolocation.getCurrentPosition(function(position) {
-//        var pos = {
-//          lat: position.coords.latitude,
-//          lng: position.coords.longitude
-//        };
-//        infoWindow.setPosition(pos);
-//        infoWindow.setContent('Location found.');
-//        infoWindow.open(campusMap);
-//        campusMap.setCenter(pos);
-//      }, function() {
-//        handleLocationError(true, infoWindow, campusMap.getCenter());
-//      });
-//    }
-//    else {
-//      handleLocationError(false, infoWindow, campusMap.getCenter());
-//    }
   }
 
- function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
- }
+  function placeMarkerAndPan(latLng, campusMap) {
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: campusMap
+    });
+    campusMap.panTo(latLng);
+  }
+
 
 /* Create a new marker for each event
  * @param event - event object
@@ -50,5 +35,14 @@ function createMarker(event,campusMap) {
     title: event.eventTitle,
     position: eventPosition
   })
+}
+
+function createEventPlacementMap() {
+  var princetonLatLng = {lat: 40.3428452, lng: -74.6568153};
+  const campusMap = new google.maps.Map(
+    document.getElementById('map'),
+    {center: princetonLatLng, zoom: 16});
+
+
 }
 
