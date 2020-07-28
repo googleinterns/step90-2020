6b5fac9bab74aff18a120cd4683c1dc67ddb7311
Jenny Sheng <jennysheng@google.com>
2020-07-28 17:25:01 -0400

    fixes university form bug

--- capstone/src/main/resources/static/user.js
+++ capstone/src/main/resources/static/user.js
@@ -113,6 +113,7 @@ function createIndividualProfile(data, fillForm) {
     document.getElementById("ind-firstname").value = data.firstName;
     document.getElementById("ind-lastname").value = data.lastName;
     document.getElementById("university-form-display").innerText = data.university.name;
+    document.getElementById("ind-uni").value = data.university.name;
   }
 }
 
@@ -133,6 +134,7 @@ function createOrgProfile(data, fillForm) {
     document.getElementById("org-form-name").value = data.name;
     document.getElementById("org-university-form-display").innerText = data.university.name;
     document.getElementById("org-description").value = data.description;
+    document.getElementById("org-uni").value = data.university.name;
   }
 }
 
